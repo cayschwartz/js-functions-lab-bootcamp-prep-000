c7839d6110e6fd30d9b2fe9cab888ac99bd1b133
Cayla Schwartz <cayschwartz@gmail.com>
2017-08-29 18:40:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 function happyHolidays(){
   return "Happy holidays!"
-  
 }
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
-  
 }
 
 function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
-  
 }
